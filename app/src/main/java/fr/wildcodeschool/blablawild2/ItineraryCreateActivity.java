658b3bb79cc4adba4f5de4f6801d449ea989b5aa
Amaury <amaury.pichon@hotmail.fr>
2018-12-09 21:08:31 +0100

    Solution de la quête "Firebase 1"

--- app/src/main/java/fr/wildcodeschool/blablawild2/ItineraryCreateActivity.java
+++ app/src/main/java/fr/wildcodeschool/blablawild2/ItineraryCreateActivity.java
@@ -1,13 +1,21 @@
 package fr.wildcodeschool.blablawild2;
 
 import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -45,6 +53,24 @@ public class ItineraryCreateActivity extends AppCompatActivity {
                     ItineraryModel itineraryModel = new ItineraryModel(departure, destination, driver, mDate, Integer.parseInt(price));
                     // TODO : save itineraryModel into Firebase
 
+                    FirebaseDatabase database = FirebaseDatabase.getInstance();
+                    DatabaseReference itineraryRef = database.getReference("itineraries");
+                    String key  = itineraryRef.push().getKey();
+
+                    itineraryRef.child(key).addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                            Intent mainIntent = new Intent(ItineraryCreateActivity.this, MainActivity.class);
+                            startActivity(mainIntent);
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError databaseError) {
+                            Toast.makeText(ItineraryCreateActivity.this, "Failed to read value.", Toast.LENGTH_LONG).show();
+                        }
+                    });
+
+                    itineraryRef.child(key).setValue(itineraryModel);
                 }
             }
         });
